1e4678d49023f3b3898b7c5d3a7bdb3bd1bfa66f
johnlloydgarcia082906-cloud <johnlloydgarcia082906@gmail.com>
2025-09-08 13:41:16 +0800

    Add files via upload

---
+++ activity2.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+
+string FullNameee, CourseYearSeccc, Placeee , Hobbies;
+
+//input
+cout << “Enter your Full Name: “;
+getline (cin, FullNameee);
+
+cout << “Enter your Course, Year and Section: “;
+getline (cin, CourseYearSeccc);
+
+cout << “Enter your Birthday: “;
+getline (cin, Placeee);
+
+cout << “Enter your Motto/Motivation in life;
+getline (cin, Hobbies);
+
+//output
+
+cout << “Hi! I’m “ << FullNameee << “of “ << CourseYearSeccc << “. Welcome to Data Structure and Algorithm!” << endl;
+cout << “ I lived at  “ << Placeee << “. And my Hobbies in life is “ << Hobbies << “.” endl;
+
+return 0;
+}
